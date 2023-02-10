a41f3c2ac6326e5cfcc74478af5b954079440868
munyeshaka <aimablemunyeshaka30@gmail.com>
2023-02-10 18:46:20 +0200

    policy

---
+++ policyAPI/urls.py
@@ -0,0 +1,11 @@
+from django.urls import re_path
+
+
+from .import views
+
+app_name = 'policyAPI'
+
+urlpatterns = [
+    re_path(r'^$', views.home),
+    re_path(r'^selah/', views.selah, name="selah"),
+]
